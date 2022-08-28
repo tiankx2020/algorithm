c878c0fc871c1973fe32fab554d27616f71ff5c7
boy <1084351114@qq.com>
2022-08-28 11:39:31 +0800

    8.28-three

--- src/com/tkx/java/dongboy/prefet/Three.java
+++ src/com/tkx/java/dongboy/prefet/Three.java
@@ -9,23 +9,23 @@ import java.util.Scanner;
 public class Three {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int[] num1 = new int[n + 2];
-        int[] num2 = new int[n + 2];
-        int[] num3 = new int[n + 2];
-        int[] num4 = new int[n + 2];
-        for (int i = 1; i <= n; i++)
+        int N = scanner.nextInt();
+        int[] num1 = new int[N + 2];
+        int[] num2 = new int[N + 2];
+        int[] num3 = new int[N + 2];
+        int[] num4 = new int[N + 2];
+        for (int i = 1; i <= N; i++)
             num1[i] = scanner.nextInt();
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= N; i++) {
             num2[i] = Math.max(num2[i - 1], 0) + num1[i];
             num3[i] = Math.max(num3[i - 1], num2[i]);
         }
-        for (int i = n; i >= 1; i--) {
+        for (int i = N; i >= 1; i--) {
             num2[i] = Math.max(num2[i + 1], 0) + num1[i];
             num4[i] = Math.max(num4[i + 1], num2[i]);
         }
-        int res = num3[n];
-        for (int i = 1; i < n; i++)
+        int res = num3[N];
+        for (int i = 1; i < N; i++)
             res = Math.max(res, num3[i] + num4[i + 1]);
         System.out.println(res);
     }
