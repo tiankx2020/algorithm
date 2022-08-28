fc9b46beb7f95282d218e8f2ffa15565acacc246
boy <1084351114@qq.com>
2022-08-28 10:26:56 +0800

    8.28-one

--- src/com/tkx/java/dongboy/prefet/One.java
+++ src/com/tkx/java/dongboy/prefet/One.java
@@ -1,6 +1,9 @@
 package com.tkx.java.dongboy.prefet;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.PriorityQueue;
+import java.util.Scanner;
 
 /**
  * @author dong boy
@@ -8,24 +11,48 @@ import java.util.PriorityQueue;
  */
 public class One {
     public static void main(String[] args) {
-        System.out.println(one(new int[]{2, 1, 9, 8, 4, 2, 5}));
-        System.out.println(one(new int[]{30, 45, 12, 23, 8}));
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        // 把结果放在容器
+        List<int[]> list = new ArrayList<>();
+        for(int i = 1; i <= t; i++){
+            int n = scanner.nextInt();
+            scanner.nextLine();
+            String str = scanner.nextLine();
+            String[] s = str.split(" ");
+            list.add(maxProduct(s));
+        }
+        for(int[] data : list){
+            System.out.println(data[0] + " " + data[1]);
+        }
     }
 
-    // 方法体
-    public static int one(int[] nums) {
-        int n = nums.length;
-        if (n == 1) {
-            return nums[0];
+    public static int[] maxProduct(String[] s) {
+        int[] res = new int[2];
+        int n = s.length;
+        int[] nums = new int[n];
+        for(int i = 0; i < n; i++){
+            nums[i] = Integer.parseInt(s[i]);
         }
-        PriorityQueue<Integer> queue = new PriorityQueue<>((a, b) -> b - a);
-        for (int data : nums) {
-            queue.offer(data);
+        // 双指针
+        int left = 0,right = 0;
+        int max = 0;
+        int temp = 1;
+        while(right < n){
+            if(nums[right] != 0){ // 不为0，把right乘进去
+                temp *= nums[right];
+                if(temp > max){ // 如果更大，就更新起始坐标
+                    max = temp;
+                    res[0] = left + 1;
+                    res[1] = right + 1;
+                }
+                right++;
+            } else { // 为0，更新起始坐标
+                right++;
+                left = right;
+                temp = 1;
+            }
         }
-        while (queue.size() > 1) {
-            int temp = queue.poll() - queue.poll();
-            queue.offer(temp);
-        }
-        return queue.poll();
+        return res;
     }
 }
