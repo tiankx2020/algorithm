2136571a8c023a66cde3c614faa602d13e4a5203
tkx <2633385761@qq.com>
2022-08-28 12:24:14 +0800

    整理

--- src/com/tkx/java/基础/AbstractClassDemo.java
+++
@@ -1,9 +0,0 @@
-package com.tkx.java.基础;
-
-/**
- * @Description: TODO
- * @author: scott
- * @date: 2022年05月10日 15:03
- */
-public  abstract class AbstractClassDemo extends Object {
-}
--- src/com/tkx/java/基础/接口/InterfaceDemo1.java
+++
@@ -1,6 +0,0 @@
-package com.tkx.java.基础.接口;
-
-public interface InterfaceDemo1 {
-    public void method01();
-    public void method02();
-}
