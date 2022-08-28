ec0d0e39c259932a99d12b491c7c8542752b7c29
tkx <2633385761@qq.com>
2022-08-28 12:27:44 +0800

    字节跳动笔试题

---
+++ src/com/tkx/java/秋招笔试题/byteDance/T1.java
@@ -0,0 +1,68 @@
+package com.tkx.java.秋招笔试题.字节跳动;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @Description: TODO
+ * @author: scott
+ * @date: 2022年08月28日 10:30
+ */
+public class T1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][10005];
+        int[] num = new int[n];
+        for (int i = 0; i < n; i++) {
+            int m = sc.nextInt();
+            num[i] = m;
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        int[][] res = new int[n][2];
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(1,0);
+        map.put(2,1);
+        map.put(4,2);
+        map.put(8,3);
+        map.put(16,4);
+        map.put(32,5);
+        map.put(64,6);
+        map.put(128,7);
+        map.put(256,8);
+        map.put(512,9);
+        map.put(1024,10);
+        for (int i = 0; i < n; i++) {
+            int start=0,end =0;
+            int maxStart = 0,maxEnd = 0;
+            int max = 0;
+            int tmp = 0;
+            for (int j = 0; j < num[i]; j++) {
+                if(arr[i][j]==0){
+                    start = j+1;
+                    end = j+1;
+                    tmp = 1;
+                }else{
+                    tmp+=map.get(arr[i][j]);
+                    end = j;
+                }
+                if(tmp>max){
+                    while (arr[i][j]==1) end--;
+                    max = tmp;
+                    maxStart = start;
+                    maxEnd = end;
+                }
+            }
+            res[i][0] = maxStart+1;
+            res[i][1] = maxEnd+1;
+        }
+        for(int i=0;i<res.length;i++){
+            System.out.println(res[i][0]+" "+res[i][1]);
+        }
+    }
+
+}
---
+++ src/com/tkx/java/秋招笔试题/byteDance/T2.java
@@ -0,0 +1,80 @@
+import java.util.*;
+
+/**
+ * @Description: TODO
+ * @author: scott
+ * @date: 2022年08月28日 10:03
+ */
+public class T2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.valueOf(sc.nextLine());
+        String[] s1 = sc.nextLine().split(" ");
+        int[] arr = new int[s1.length];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.valueOf(s1[i]);
+        }
+        List<int[]> tmp = new ArrayList<>();
+        for (int i = 1; i < n; i++) {
+            String[] s = sc.nextLine().split(" ");
+            int a = Integer.valueOf(s[0]);
+            for(int j=1;j<s.length;j++){
+                int b = Integer.valueOf(s[j]);
+                tmp.add(new int[]{a,b});
+            }
+        }
+        int[][] preArr =  new int[tmp.size()][2];
+        for(int i=0;i< preArr.length;i++){
+            preArr[i] = tmp.get(i);
+        }
+        int[] res = new int[arr.length];
+        for(int i=0;i< arr.length;i++){
+            res[i]= method01(arr[i],preArr);
+        }
+        for (int re : res) {
+            System.out.print(re+" ");
+        }
+
+    }
+
+    public static int method01(int index, int[][] preArr) {
+        //记录入度为0的节点
+        Queue<Integer> queue = new ArrayDeque<>();
+        //入度数量  依赖什么
+        int[] indegre = new int[index+100];
+        //出度元素   被依赖
+        List<Integer>[] lists = new List[index+100];
+
+        for (int i = 0; i < lists.length; i++) {
+            lists[i] = new ArrayList<Integer>();
+        }
+
+        int num = 0;
+        for (int[] arr : preArr) {
+            //入度加1
+            indegre[arr[0]]++;
+            lists[arr[1]].add(arr[0]);
+        }
+
+        for (int i = 0; i < indegre.length; i++) {
+            if (indegre[i] == 0) {
+                queue.add(i);
+            }
+        }
+        while (queue.size() > 0) {
+            int tmp = queue.poll();
+            num++;
+            for (int x : lists[tmp]) {
+                indegre[x]--;
+                if (indegre[x] == 0) {
+                    queue.add(x);
+                }
+            }
+        }
+        if(num== index){
+            return 1;
+        }else{
+            return  0;
+        }
+    }
+}
