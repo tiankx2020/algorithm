159efaa0950c54ae59dd0fc75e5e6d1db0562331
boy <1084351114@qq.com>
2022-08-28 10:39:12 +0800

    8.28-one-update-1

--- src/com/tkx/java/dongboy/prefet/One.java
+++ src/com/tkx/java/dongboy/prefet/One.java
@@ -2,7 +2,6 @@ package com.tkx.java.dongboy.prefet;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.PriorityQueue;
 import java.util.Scanner;
 
 /**
@@ -15,44 +14,36 @@ public class One {
         int t = scanner.nextInt();
         // 把结果放在容器
         List<int[]> list = new ArrayList<>();
-        for(int i = 1; i <= t; i++){
+        for (int x = 1; x <= t; x++) {
             int n = scanner.nextInt();
             scanner.nextLine();
-            String str = scanner.nextLine();
-            String[] s = str.split(" ");
-            list.add(maxProduct(s));
+            int[] res = new int[2];
+            int[] nums = new int[n];
+            for(int i = 0;  i < n; i++){
+                nums[i] = scanner.nextInt();
+            }
+            int left = 0, right = 0;
+            int max = 0;
+            int temp = 1;
+            while (right < n) {
+                if (nums[right] != 0) { // 不为0，把right乘进去
+                    temp *= nums[right];
+                    if (temp > max) { // 如果更大，就更新起始坐标
+                        max = temp;
+                        res[0] = left + 1;
+                        res[1] = right + 1;
+                    }
+                    right++;
+                } else { // 为0，更新起始坐标
+                    right++;
+                    left = right;
+                    temp = 1;
+                }
+            }
+            list.add(res);
         }
-        for(int[] data : list){
+        for (int[] data : list) {
             System.out.println(data[0] + " " + data[1]);
         }
-    }
-
-    public static int[] maxProduct(String[] s) {
-        int[] res = new int[2];
-        int n = s.length;
-        int[] nums = new int[n];
-        for(int i = 0; i < n; i++){
-            nums[i] = Integer.parseInt(s[i]);
-        }
-        // 双指针
-        int left = 0,right = 0;
-        int max = 0;
-        int temp = 1;
-        while(right < n){
-            if(nums[right] != 0){ // 不为0，把right乘进去
-                temp *= nums[right];
-                if(temp > max){ // 如果更大，就更新起始坐标
-                    max = temp;
-                    res[0] = left + 1;
-                    res[1] = right + 1;
-                }
-                right++;
-            } else { // 为0，更新起始坐标
-                right++;
-                left = right;
-                temp = 1;
-            }
-        }
-        return res;
     }
 }
