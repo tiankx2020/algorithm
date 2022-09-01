bddcc83f65e6d71ae44dfed0d27832e5cb16ed03
tkx <2633385761@qq.com>
2022-09-01 20:28:49 +0800

    深信服笔试

--- src/com/tkx/java/秋招笔试题/深信服/T1.java
+++ src/com/tkx/java/秋招笔试题/深信服/T1.java
@@ -6,15 +6,32 @@ package com.tkx.java.秋招笔试题.深信服;
  * @date: 2022年09月01日 19:37
  */
 public class T1 {
-    /**
-     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
-     *
-     *
-     * @param grid int整型二维数组
-     * @return int整型
-     */
-    public int ncov_defect (int[][] grid) {
-        // write code here
-        return  0;
+    public int ncov_defect(int[][] grid) {
+        int m = grid.length;
+        int res = 0;
+        for (int i = 0; i < m; i++) {
+            int n = grid[i].length;
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    if (i - 1 >= 0 && grid[i - 1][j] == 0) {
+                        grid[i - 1][j] = -1;
+                        res++;
+                    }
+                    if (i + 1 < m && grid[i + 1][j] == 0) {
+                        grid[i + 1][j] = -1;
+                        res++;
+                    }
+                    if (j - 1 >= 0 && grid[i][j - 1] == 0) {
+                        grid[i][j - 1] = -1;
+                        res++;
+                    }
+                    if (j + 1 < n && grid[i][j + 1] == 0) {
+                        grid[i][j + 1] = -1;
+                        res++;
+                    }
+                }
+            }
+        }
+        return res;
     }
 }
--- src/com/tkx/java/秋招笔试题/深信服/T3.java
+++ src/com/tkx/java/秋招笔试题/深信服/T3.java
@@ -8,8 +8,48 @@ import com.tkx.java.ListNode;
  * @date: 2022年09月01日 19:37
  */
 public class T3 {
-    public ListNode reverseBetween (ListNode head) {
+    public ListNode reverseBetween(ListNode head) {
         // write code here
-        return null;
+        int index = 0;
+        ListNode p = head;
+        ListNode p2 = null;
+        ListNode p3 = null;
+        ListNode p6 = null;
+        ListNode p7 = null;
+        while (true) {
+            index++;
+            if (index == 2) {
+                p2 = p;
+            }
+            if (index == 3) {
+                p3 = p;
+            }
+            if (index == 6) {
+                p6 = p;
+            }
+            if (index == 7) {
+                p7 = p;
+            }
+            if (index == 8) {
+                break;
+            }
+            p = p.next;
+        }
+        reverse(p3,p7);
+        p2.next = p6;
+        p3.next = p7;
+        return head;
+    }
+
+    public void reverse(ListNode node, ListNode end) {
+        ListNode pre = null;
+        ListNode cur = node;
+        ListNode tmp = null;
+        while (cur != end){
+            tmp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = tmp;
+        }
     }
 }
